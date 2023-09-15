7a4cbf109cae82f770646ca4d1b3e2da6aa08f9e
Goat95 <hjh9703@naver.com>
2023-09-15 19:34:46 +0900

    feat: implements projects, project components

---
+++ components/Projects.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import { projectsData } from "@/lib/data";
+import SectionHeading from "./SectionHeading";
+import Project from "./Project";
+
+export default function Projects() {
+  return (
+    <section>
+      <SectionHeading>My projects</SectionHeading>
+      <div>
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
+      </div>
+    </section>
+  );
+}
